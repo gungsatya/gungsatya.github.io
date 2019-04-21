29026e2895078eb5a0ce365c141090a3474bd193
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2019-04-21 13:37:35 +0800

    update

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,19 +2,28 @@
 //  Created for gungsatya.github.io
 //  September 25th, 2018
 // 
-$('.img-pop-up').magnificPopup({
+
+window.onload = function () {
+  $('.img-pop-up').magnificPopup({
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     mainClass: 'mfp-img-mobile',
-    gallery:{
-        enabled:true,
-        navigateByImgClick: true,
-        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     },
     image: {
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-      titleSrc: function(item) {
+      titleSrc: function (item) {
         return item.el.attr('title') + '<small>by Satya Wibawa</small>';
       }
     }
-});
+  });
+  var typed = new Typed('#typed', {
+    strings: ["Satya Wibawa", "A Programmer"],
+    smartBackspace: true, // Default value
+    loop: true,
+    typeSpeed: 200
+  });
+}
